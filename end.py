adf23cbbc3916a79dbb379a38b190d123f240dc8
neph <neph3779@gmail.com>
2023-10-29 16:54:16 +0900

    :sparkles: script 수정

--- end.py
+++ end.py
@@ -5,7 +5,7 @@ now = datetime.datetime.now()
 end_date = now.strftime('%Y-%m-%d')
 
 # 커밋 및 푸시
-commit_message = f":book: {end_date} 공부 종료"
+commit_message = f":book: {end_date} 공부 정리자료 업로드"
 os.system(f"git add .")
 os.system(f"git commit -m '{commit_message}'")
 os.system("git push origin main")
--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import subprocess
 
 now = datetime.datetime.now()
 start_date = now.strftime('%Y-%m-%d')
@@ -8,3 +9,6 @@ start_date = now.strftime('%Y-%m-%d')
 til_file_name = f"{start_date}.md"
 with open(til_file_name, 'w') as til_file:
     pass  # 아무 내용 없이 빈 파일 생성
+
+# 파일 열기 (Mac OS에서는 open 명령 사용)
+subprocess.call(['open', til_file_name])
